b911cc9dcc115ad9e4866b92a8e4dfc0f0acd551
John Lusk <john-github@how-hard-can-it-be.com>
2016-06-11 13:22:47 -0400

    Decorator play.

--- app/simple.ts
+++ app/simple.ts
@@ -1,3 +1,28 @@
+function foo( ctor: Function)
+{
+   console.log( `In decorator foo, ctor = ${ctor}`);
+}
+
+function bar( ctorOrPrototype: any, memberName: string, memberPropDesc: PropertyDescriptor )
+{
+   console.log( `In decorator bar, ctorOrPrototype = ${ctorOrPrototype}, memberName = ${memberName}, memberPropDesc = ${memberPropDesc}`);
+}
+
+function g()
+{
+   console.log( "g() called");
+}
+
+@foo
+class C
+{
+   @bar
+   f()
+   {
+      console.log( "f() called");
+   }
+}
+
 // -------------------------------------------------  button_1_click  --------------------------------------------------
 
 function button_1_click()
@@ -22,3 +47,11 @@ function button_1_click()
    }
 }
 
+function button_2_click()
+{
+   console.log( "Button 2 clicked...");
+   let c = new C();
+   c.f();
+   g();
+   console.log( "Button 2 click handler done.");
+}
